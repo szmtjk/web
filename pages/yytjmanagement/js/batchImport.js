70ce0b8d2bacbc7af530f4f13f8511a0cdf31e31
litao <litao@carzone.top>
2019-04-14 10:45:00 +0800

    文件上传修改

--- pages/yytjmanagement/js/batchImport.js
+++ pages/yytjmanagement/js/batchImport.js
@@ -67,9 +67,16 @@ var PageYytjImport = function(){
                 dataType: 'json',
                 onSend: function ($this, str) {
                     // $.jBox.tip("正在上传附件，请稍后", 'loading');
+                    var tmp = '正在上传附件，请稍后...';
+                    mini.mask({
+                        el: document.body,
+                        cls: 'mini-mask-loading',
+                        html: tmp
+                    });
                     return true;
                 },
                 onComplate: function (data) {
+                    mini.unmask(document.body);
                     if(data.success && data.data){
                         PageMain.funShowMessageBox("上传成功");
                         that.dataYytjGrid.reload();
@@ -81,6 +88,37 @@ var PageYytjImport = function(){
             });
             $this.upload("ajaxSubmit");
         },
+        funcDoUpload: function () {
+            var htmlContent = '<div id="htmlContent" style="padding:10px;height: 150px;"><input class="upload-file" name="zipFile" title="点击上传" type="file" value="上传" accept=""></div>';
+            mini.showMessageBox({
+                width: 400,
+                title: "文件上传",
+                buttons: ["ok", "cancel"],
+                message: "文件上传",
+                html: htmlContent,
+                showModal: false,
+                callback: function (action) {
+                    var file = $('.upload-file');
+                    if (action == 'ok') {
+                        if (file.val()) {
+                            PageYytjImport.funImageAjaxSubmit(file, "", "details_add");
+                        } else {
+                            PageMain.funShowMessageBox("请选择文件");
+                            // mini.showTips({
+                            //     content: "请选择文件",
+                            //     state: "warning",
+                            //     timeout: 3000
+                            // });
+                            return false;
+                        }
+                    }
+                    // $('.upload-file').change(function () {
+                    //     PageYytjImport.funImageAjaxSubmit($(this), "", "details_add");
+                    // });
+
+                }
+            });
+        }
     }
 }();
 
